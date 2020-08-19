485ed957c670e61e911021fd57e2bec5561daf35
anassohail99 <anas.sohailabs1@gmail.com>
2020-08-20 00:49:15 +0500

    Apollo Client and provider added

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
 import React from "react";
 import "./App.css";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import logo from "./assets/spacex.svg";
+import Launches from "./components/Launches";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+});
 
 function App() {
-  return <div className="App">SpaceX</div>;
+  return (
+    <ApolloProvider client={client}>
+      <div className="container">
+        <div className="logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <Launches />
+      </div>
+    </ApolloProvider>
+  );
 }
 
 export default App;
