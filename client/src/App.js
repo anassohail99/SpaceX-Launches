1427288f41439424cd9db880ac75035c933bc154
anassohail99 <anas.sohailabs1@gmail.com>
2020-08-21 00:17:53 +0500

    Routing added

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,10 @@ import React from "react";
 import "./App.css";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import logo from "./assets/spacex.svg";
 import Launches from "./components/Launches";
+import Launch from "./components/Launch";
 
 const client = new ApolloClient({
   uri: "http://localhost:5000/graphql",
@@ -12,12 +14,15 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="container">
-        <div className="logo">
-          <img src={logo} alt="logo" />
+      <Router>
+        <div className="container">
+          <div className="logo">
+            <img src={logo} alt="logo" />
+          </div>
+          <Route exact path="/" component={Launches} />
+          <Route exact path="/launch/:flight_number" component={Launch} />
         </div>
-        <Launches />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
