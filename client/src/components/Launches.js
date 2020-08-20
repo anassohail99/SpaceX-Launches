2151ba906028d10bc81eb4adac2a8d971bb77e35
anassohail99 <anas.sohailabs1@gmail.com>
2020-08-21 00:19:26 +0500

    loading component added

--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -2,9 +2,8 @@ import React, { Fragment } from "react";
 import gql from "graphql-tag";
 import { Query } from "react-apollo";
 import LaunchItem from "./LaunchItem";
-import { makeStyles } from "@material-ui/core/styles";
-import CircularProgress from "@material-ui/core/CircularProgress";
 import MissionKey from "./MissionKey";
+import Loader from "./Loader";
 
 // creating query
 
@@ -20,16 +19,6 @@ const LAUNCHES_QUERY = gql`
 `;
 
 const Launches = () => {
-  const useStyles = makeStyles({
-    spinner: {
-      position: "absolute",
-      top: "50%",
-      left: "50%",
-    },
-  });
-
-  const classes = useStyles();
-
   return (
     <Fragment>
       <h1 className="display-4 my-5" style={{ fontSize: 45, marginTop: 20 }}>
@@ -39,10 +28,7 @@ const Launches = () => {
       <MissionKey />
       <Query query={LAUNCHES_QUERY}>
         {({ loading, error, data }) => {
-          if (loading)
-            return (
-              <CircularProgress color="secondary" className={classes.spinner} />
-            );
+          if (loading) return <Loader />;
           if (error) console.error(error);
 
           return (
